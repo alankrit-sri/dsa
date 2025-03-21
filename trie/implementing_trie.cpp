b12f5cdb869eec187d57757493899a14a749e302
System Administrator <root@Alankrits-MacBook-Air.local>
2025-03-21 06:31:17 +0530

    Example of implementing a trie also known as prefix tree, three functions are implemented word search, prefix search and insert

---
+++ trie/implementing_trie.cpp
@@ -0,0 +1,66 @@
+class TrieNode{
+    public:
+    unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+    TrieNode()
+    {
+        isEndOfWord = false;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        for(auto ch : word)
+        {
+            if(node->children.find(ch) == node->children.end())
+            {
+                node->children[ch] = new TrieNode();
+            }
+            node = node->children[ch];
+        }
+        node->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        for(auto ch : word)
+        {
+            if(node->children.find(ch) == node->children.end())
+            {
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return node->isEndOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for(auto ch : prefix)
+        {
+            if(node->children.find(ch) == node->children.end())
+            {
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
