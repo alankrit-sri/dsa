99168a0cd05f438f7c93da70f5d76bfb36dc1287
System Administrator <root@Alankrits-MacBook-Air.local>
2025-04-22 23:11:46 +0530

    Reversing Linked List using Tail recursion

---
+++ linked-list/reverse_linked_list_recursion.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* reverseRecursive(ListNode* current, ListNode* prev) {
+        if (!current) return prev;
+        
+        ListNode* nextNode = current->next;
+        current->next = prev;
+        
+        return reverseRecursive(nextNode, current);
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+        return reverseRecursive(head, nullptr);
+    }
+};
